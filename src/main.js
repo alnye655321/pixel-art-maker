79c1bb1324d90179a1a16956870beb11b30a26fa
Alex Nye <alnye655321@gmail.com>
2016-07-21 11:28:55 -0600

    changed structure

--- src/main.js
+++ src/main.js
@@ -3,13 +3,25 @@
   function changeEventListner(color){
     var classname = document.getElementsByClassName("pixel");
 
+    var changeColorBlue = function() {
+        //var attribute = this.getAttribute("data-myattribute");
+        this.style.backgroundColor = "blue";
+
+    };
+
+
     for (var i = 0; i < classname.length; i++) {
-        classname[i].addEventListener('click', changeColor, false);
-        classname[i].addEventListener('click', changeColor, false);
+        classname[i].removeEventListener('click', changeColor);
+        classname[i].addEventListener('click', changeColorBlue, false);
+
     }
 
   }
+  var blueButton = document.getElementById("blue");
 
+  blueButton.addEventListener('click', changeEventListner("blue"), false);
+
+// initial creation of div boxes
 function newCanvas(){
 var pixel;
 var canvas = document.getElementById('canvas');
@@ -38,6 +50,11 @@ var canvas = document.getElementById('canvas');
 }
 newCanvas();
 
+//reset all event listners http://stackoverflow.com/questions/19469881/remove-all-event-listeners-of-specific-type
+// var el = document.getElementById('el-id'),
+//     elClone = el.cloneNode(true);
+//
+// el.parentNode.replaceChild(elClone, el);
 
 
 
